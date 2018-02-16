4bcb835c0f929d8c2e1fb1d79d75db6df68d97c1
Annapurna <poornarao266@gmail.com>
2018-02-16 22:21:11 +0530

    Add files via upload

--- youbetchaican/src/app/components/product/product.jsx
+++ youbetchaican/src/app/components/product/product.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+const productImg = require('../../../assets/product.jpg');
 export default class Product extends Component {
 
     constructor(props) {
@@ -33,15 +33,15 @@ export default class Product extends Component {
 
         return (
             <div className="card">
-                <img className="card-img-top" src="http://placehold.it/350x300" alt="Card image cap"/>
+                <img className="card-img-top" src={productImg} alt="Card image cap"/>
                 <div className="card-body">
                     <h5 className="card-title">Product title</h5>
                     <h6 className="card-title">&#x20B9; 00.00</h6>
                     <p className="card-text text-muted fwl">This is a wider card with supporting text below as a natural lead-in to
                         additional content. This content is a little bit longer.</p>
-                    <div className={'text-center'}>
-                        <a href="#" className="btn btn-primary ">Buy Now</a>
-                    </div>
+                </div>
+                <div className="card-footer p-0">
+                    <a href="#" className="btn btn-primary btn-block btn-buy">Buy Now</a>
                 </div>
 
             </div>
